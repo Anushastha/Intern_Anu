8a280872e3357babf0d39d7f5a4630f56dd060dd
Anushastha <shresthanu123@gmail.com>
2023-11-01 11:10:46 +0545

    travel website - upto places section

---
+++ jsCodes/landing.js
@@ -0,0 +1,9 @@
+// const textfield = document.getElementById("dest");
+
+// textfield.addEventListener("input", () => {
+//   if (textfield.value) {
+//     document.querySelector(".arrow-icon").style.visibility = "visible";
+//   } else {
+//     document.querySelector(".arrow-icon").style.visibility = "hidden";
+//   }
+// });
