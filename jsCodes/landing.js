65b31fb517a7f89d877a32988371ee535f42b0c4
Anushastha <shresthanu123@gmail.com>
2023-11-23 21:46:49 +0545

    versel.json file

--- jsCodes/landing.js
+++
@@ -1,9 +0,0 @@
-// const textfield = document.getElementById("dest");
-
-// textfield.addEventListener("input", () => {
-//   if (textfield.value) {
-//     document.querySelector(".arrow-icon").style.visibility = "visible";
-//   } else {
-//     document.querySelector(".arrow-icon").style.visibility = "hidden";
-//   }
-// });
